7a7bbb34f364979b274f393ad35964ffa51f9f9a
Nainho1703 <iggarcia1703@gmail.com>
2024-06-11 17:32:48 +0200

    Update on results, trainig and utils

--- Assignment_2/utils/utils.py
+++ Assignment_2/utils/utils.py
@@ -16,6 +16,10 @@ from skseq.sequences.id_feature import IDFeatures
 from skseq.sequences.extended_feature import ExtendedFeatures
 from skseq.sequences.structured_perceptron import StructuredPerceptron
 from tensorflow.keras.models import load_model
+import pickle
+
+from IPython.display import display, HTML
+
 ############################################################################################################
 # No need to have this function since we do have tiny_dataset.csv provided!
 def tiny_test():
@@ -86,6 +90,10 @@ def accuracy(y_true, y_pred):
 
     # Calculate and return the accuracy score
     return accuracy_score(filtered_y_true, filtered_y_pred) if filtered_y_true else 0.0
+
+
+
+
 ############################################################################################################
 def plot_confusion_matrix(y_true, y_pred, dict_tag):
     """
@@ -115,8 +123,11 @@ def plot_confusion_matrix(y_true, y_pred, dict_tag):
     cm_df = pd.DataFrame(cm, index=index_labels, columns=columns_labels)
 
     # Plotting the confusion matrix
-    plt.figure(figsize=(10, 10))
-    sns.heatmap(cm_df, annot=True, fmt='d', cmap='Blues', cbar=False)
+    fig_size = max(10, 0.8*len(index_labels) / 2), 0.8*max(10, len(columns_labels) / 2)
+
+    # Plotting the confusion matrix
+    plt.figure(figsize=fig_size)
+    sns.heatmap(cm_df, annot=True, fmt='d', cmap="rocket_r",  cbar=False, xticklabels = True)
     plt.xlabel('Predicted values')
     plt.ylabel('Actual values')
     plt.title('Confusion Matrix')
@@ -207,19 +218,128 @@ def create_sequence_list(X, y, word_dict, tag_dict):
     return sequence_list
 
 
+def load_p(path):
+    """
+    Load data from a pickle file.
 
-def evaluate(data_,model,y_true,tag_dict):
+    Parameters:
+        path (str): The path to the pickle file.
 
-    predictions = []
+    Returns:
+        object: The loaded data.
+    """
+    with open(path, 'rb') as file:
+        df_ = pickle.load(file)
+    return df_
 
-    for i in tqdm(range(len(data_)), desc="Predicting tags", unit="sequence"):
-        predicted_tag = model.predict_tags_given_words(data_[i])
-        predictions.append(predicted_tag)
+def save_p(path, data):
+    """
+    Save data to a pickle file.
 
-    predictions = [np.ndarray.tolist(array) for array in predictions]
-    predictions = np.concatenate(predictions).ravel().tolist()
+    Parameters:
+        path (str): The path to save the pickle file.
+        data (object): The data to be saved.
+    """
+    with open(path, "wb") as f:
+        pickle.dump(data, f)
 
-    print(f1_score_weighted(y_true, predictions))
-    print(accuracy(y_true, predictions))
-    plot_confusion_matrix(y_true, predictions, tag_dict)
+
+
+def get_accuracy(model, data, true_tags_dict, corpus_tag_dict):
+    """
+    Calculate accuracy metrics for a given model and dataset.
+
+    Parameters:
+        model (object): The trained model object.
+        data (list): A list of sentences.
+        true_tags_dict (dict): A dictionary containing the true tags for each sentence.
+        corpus_tag_dict (dict): A dictionary mapping tags to indices.
+
+    Returns:
+        tuple: A tuple containing the predicted tags list, the number of correct sentences, the total number of correct tags, and the total number of tags.
+    """
+    total_tags = 0.0
+    correct_tags = 0.0
+    correct_sentences = 0.0
+    predicted_tags_list = []
+    
+    # For each sentence
+    for i in range(len(data)):
+        # Get the predicted tags
+        predicted_tags = model.predict_tags_given_words(data[i])
+        predicted_tags_list.append(predicted_tags)
+        
+        # For each predicted tag
+        for index, pred_tag in enumerate(predicted_tags):
+            # If both predicted and true tags are "O", ignore
+            # If both tags are equal and not "O", count as a correct prediction
+            if pred_tag == true_tags_dict[i][index] and pred_tag != corpus_tag_dict["O"]:
+                correct_tags += 1
+                # Count the tag
+                total_tags += 1
+            # When there has been a miss-prediction, sum up the total number of tags
+            elif pred_tag != true_tags_dict[i][index]:
+                total_tags += 1
+        
+        # Check if the whole sentence is exactly the same (including "O" tags)
+        if list(predicted_tags) == true_tags_dict[i]:
+            correct_sentences += 1
+    
+    return predicted_tags_list, correct_sentences, correct_tags, total_tags
+
+def evaluate(data_, model, data_tag_pos, corpus_tag_dict, y_true, name, load=False):
+    """
+    Evaluate the model's performance on the given dataset and calculate various metrics.
+
+    Parameters:
+        data_ (list): A list of sentences.
+        model (object): The trained model object.
+        data_tag_pos (dict): A dictionary containing the true tags for each sentence.
+        corpus_tag_dict (dict): A dictionary mapping tags to indices.
+        y_true (list): A list of true tags.
+        name (str): The name to use for saving results.
+        load (bool): Whether to load previously saved predictions and metrics.
+
+    Returns:
+        None
+    """
+    if load == False:
+        # Get predictions and accuracy metrics
+        list_y_pred, correct_sentence, correct, total = get_accuracy(model, data_, data_tag_pos, corpus_tag_dict)
+        y_pred = [tag for array in list_y_pred for tag in array]
+
+        # Save predictions
+        save_p("predictions/" + name, y_pred)
+    
+        # Calculate F1 score and accuracy
+        f1_score = f1_score_weighted(y_true, y_pred)
+        acc = accuracy(y_true, y_pred)
+
+        # Create a DataFrame to store metrics
+        metrics = pd.DataFrame({
+            "% of correct sentences": [correct_sentence / len(data_)],
+            "% of tags correct": [correct / total],
+            "F1 Score": [f1_score],
+            "Accuracy": [acc]
+        })
+        
+        # Save metrics to CSV file
+        metrics.to_csv("results/" + name + ".csv", index=None)
+
+    else:
+        # Load previously saved metrics and predictions
+        metrics = pd.read_csv("results/" + name + ".csv")
+        y_pred = load_p("fitted_models/predictions/" + name)
+    
+    # Display metrics
+    print("Metrics:")
+    display(HTML(round(metrics * 100, 2).to_html()))
+
+    # Create a feasible dictionary for the confusion matrix
+    feasible_dict = {k: corpus_tag_dict[k] for k in [{v: k for k, v in corpus_tag_dict.items()}[i] for i in set(y_true + y_pred)] if k in corpus_tag_dict}
+
+    # Print confusion matrix
+    print("\nConfusion Matrix\n")
+    plot_confusion_matrix(y_true, y_pred, feasible_dict)
+
 ############################################################################################################
